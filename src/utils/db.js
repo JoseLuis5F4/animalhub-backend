07737dfa7e26375d941d13a6691ae12b3248a1f5
JoseLuis5F4 <jlmpdlla@gmail.com>
2024-05-20 00:08:09 +0200

    Console log fixed

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,9 +6,9 @@ const DB_URL = process.env.DB_URL;
 const connectMongo = async () => {
   try {
     const db = await mongoose.connect(DB_URL);
-    const { host } = db.connection;
+    const { name } = db.connection;
 
-    console.log(`Conexión realizada a la base de datos con el host ${host}`);
+    console.log(`Conexión realizada a la base de datos: ${name}`);
   } catch (error) {
     console.log(`Ha habido un error al conectar con la base de datos`, error);
   }
